2fdb73c9fa983f856b4908f4028cb7fe95ac2da5
aminee-mhamdi <notqgrenix@yahoo.fr>
2020-03-13 10:59:24 +0100

    i slove the exercie

--- warmUp1.js
+++ warmUp1.js
@@ -1,6 +1,28 @@
 // 1-using + operator combine your partner first and last name .
+function fullName(firstName,LastName) {
+	return firstName + ' ' + LastName;
+}
 // 2-find if the number 13 is a multiple of 3 or not.
+function multiple(x) {
+	if(x % 3 === 0) {
+		return x ' is multiple of 3';
+	} 
+	return x ' its not a multiple of 3';
+}
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
-// 4-calculate your age in seconds.
+function averageAge (nums) {
+var total = 0
+var i = 0
+while (i < nums.length)
+total = total + nums[i]
+i = i + 1
+var result = total / 10; {
+return result ;
+}
+}
 
-// your code is here
+
+// 4-calculate your age in seconds.
+function agaeInSecends(age) {
+	return 60 * 60 * 24 * 365 * age 
+}
